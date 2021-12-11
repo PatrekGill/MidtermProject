c14a902fba3f5641689060b6749e6b7064002c14
Kyle Palasti <Kyle.Palasti@gmail.com>
2021-12-10 17:03:29 -0700

    Create Spring Boot/MVC project

---
+++ AudioFile/src/main/java/com/example/demo/AudioFileApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioFileApplication.class, args);
+	}
+
+}
