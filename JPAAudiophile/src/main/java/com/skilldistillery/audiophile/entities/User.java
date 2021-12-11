52560ddddc688581bd4f2577314c391cede8887a
PatrekGill <patrekGill@yahoo.com>
2021-12-11 10:57:07 -0700

    finished user mappings

--- JPAAudiophile/src/main/java/com/skilldistillery/audiophile/entities/User.java
+++ JPAAudiophile/src/main/java/com/skilldistillery/audiophile/entities/User.java
@@ -1,7 +1,9 @@
 package com.skilldistillery.audiophile.entities;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,33 +15,148 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="create_date")
+	private LocalDateTime creationDateTime;
+	
+	@Column(name="image_url")
+	private String imageURL;
+	
+	private boolean enabled;
+	private String email;
+	private String password;
 	private String username;
-
+//	private String role;
+	
+	/* ----------------------------------------------------------------------------
+		Constructors
+	---------------------------------------------------------------------------- */
 	public User() {
 		super();
 	}
-
+	
+	/* ----------------------------------------------------------------------------
+		get/set Id
+	---------------------------------------------------------------------------- */
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
+	
+	
+	/* ----------------------------------------------------------------------------
+		get/set Username
+	---------------------------------------------------------------------------- */
 	public String getUsername() {
 		return username;
 	}
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	
 
+	/* ----------------------------------------------------------------------------
+		get/set FirstName
+	---------------------------------------------------------------------------- */
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	/* ----------------------------------------------------------------------------
+		get/set LastName
+	---------------------------------------------------------------------------- */
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	/* ----------------------------------------------------------------------------
+		get/set CreationDateTime
+	---------------------------------------------------------------------------- */
+	public LocalDateTime getCreationDateTime() {
+		return creationDateTime;
+	}
+	public void setCreationDateTime(LocalDateTime creationDateTime) {
+		this.creationDateTime = creationDateTime;
+	}
+
+	
+	/* ----------------------------------------------------------------------------
+		get/set CreationDateTime
+	---------------------------------------------------------------------------- */
+	public String getImageURL() {
+		return imageURL;
+	}
+	public void setImageURL(String imageURL) {
+		this.imageURL = imageURL;
+	}
+
+	
+	/* ----------------------------------------------------------------------------
+		get/set Enabled
+	---------------------------------------------------------------------------- */
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	
+	/* ----------------------------------------------------------------------------
+		get/set Email
+	---------------------------------------------------------------------------- */
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	/* ----------------------------------------------------------------------------
+		get/set Password
+	---------------------------------------------------------------------------- */
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+	/* ----------------------------------------------------------------------------
+		get/set Role
+	---------------------------------------------------------------------------- */
+//	public String getRole() {
+//		return role;
+//	}
+//	public void setRole(String role) {
+//		this.role = role;
+//	}
+
+	/* ----------------------------------------------------------------------------
+		misc
+	---------------------------------------------------------------------------- */
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -51,12 +168,10 @@ public class User {
 		User other = (User) obj;
 		return id == other.id;
 	}
-
+	
 	@Override
 	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("User [id=").append(id).append(", username=").append(username).append("]");
-		return builder.toString();
+		return "User [id=" + id + ", username=" + username + "]";
 	}
 
 }
