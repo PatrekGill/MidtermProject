e8caa23c02b5cac42e229882d3184c985cb0b614
PatrekGill <patrekGill@yahoo.com>
2021-12-11 13:11:13 -0700

    added album to user test

--- JPAAudiophile/src/test/java/com/skilldistillery/audiophile/entities/AlbumTest.java
+++ JPAAudiophile/src/test/java/com/skilldistillery/audiophile/entities/AlbumTest.java
@@ -1,6 +1,9 @@
 package com.skilldistillery.audiophile.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -68,5 +71,15 @@ class AlbumTest {
 	void test_UserId_basic_mappings() {
 		assertNotNull(album);
 		assertEquals(1, album.getUserId());
+	}
+	
+	@Test
+	void test_favoritedBy_mapping() {
+		assertNotNull(album);
+		assertNotNull(album.getFavoritedBy());
+		
+		assertFalse(album.getFavoritedBy().isEmpty());
+		assertEquals(1,album.getFavoritedBy().get(0).getId());
+		assertEquals("admin",album.getFavoritedBy().get(0).getUsername());
 	}
 }
