363a6f734fc1be11bd5f148d7d8357e52193b6da
Daniel Faulkner <danfaulkner0@gmail.com>
2021-12-11 15:58:11 -0700

    updated & fixed SongTest

--- JPAAudiophile/src/test/java/com/skilldistillery/audiophile/entities/SongTest.java
+++ JPAAudiophile/src/test/java/com/skilldistillery/audiophile/entities/SongTest.java
@@ -48,9 +48,9 @@ class SongTest {
 	@DisplayName("test song mapping")
 	void test1() {
 		assertNotNull(song);
-		assertEquals("Stories We Could Tell", song.getName());
-		assertTrue(song.getLyrics().contains("Talkin' to myself againTalkin' to myself again"));
-		assertEquals(198, song.getDurationInSeconds());
+		assertEquals("Door Number Three", song.getName());
+		assertTrue(song.getLyrics().contains("Oh I took a wrong turn, it was the right"));
+		assertEquals(180, song.getDurationInSeconds());
 		assertEquals(1, song.getUser().getId());
 	}
 	
@@ -81,13 +81,14 @@ class SongTest {
 		assertEquals("admin", song.getUser().getUsername());
 		assertEquals("admin@gmail.com", song.getUser().getEmail());
 	}
+	
 	@Test
 	@DisplayName("test song to create date ")
 	void test5() {
 		assertNotNull(song);
 		assertNotNull(song.getUser());
 		assertEquals("admin", song.getUser().getUsername());
-		assertEquals(5, song.getCreateDate().getDayOfMonth());
+		assertEquals(2, song.getCreateDate().getDayOfMonth());
 		assertEquals(2019, song.getCreateDate().getYear());
 		assertEquals(6, song.getCreateDate().getMonthValue());
 	}
@@ -100,5 +101,13 @@ class SongTest {
 		assertFalse(song.getFavoritedBy().isEmpty());
 		assertEquals(1,song.getFavoritedBy().get(0).getId());
 		assertEquals("admin",song.getFavoritedBy().get(0).getUsername());
+	}
+	
+	@Test
+	@DisplayName("test song to song_rating mapping")
+	void test6() {
+		assertNotNull(song);
+		assertNotNull(song.getSongRatings());
+		assertTrue(song.getSongRatings().size() > 0);
 	}
 }
