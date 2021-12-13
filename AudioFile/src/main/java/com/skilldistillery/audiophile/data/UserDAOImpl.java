56994115239b5afecddb2b590530fe7cdbdbdc07
PatrekGill <patrekGill@yahoo.com>
2021-12-13 13:04:03 -0700

    updated UserDAO with several methods and created tests. Also fixed error in song rating impl.

--- AudioFile/src/main/java/com/skilldistillery/audiophile/data/UserDAOImpl.java
+++ AudioFile/src/main/java/com/skilldistillery/audiophile/data/UserDAOImpl.java
@@ -1,5 +1,8 @@
 package com.skilldistillery.audiophile.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
@@ -14,17 +17,82 @@ public class UserDAOImpl implements UserDAO {
 	
 	@PersistenceContext
 	private EntityManager em;
+	
+	/* ----------------------------------------------------------------------------
+		findByUsername
+	---------------------------------------------------------------------------- */
 	@Override
 	public User findByUsername(String username) {
-		// TODO Auto-generated method stub
-		String jpql ="SELECT u from User u where u.username =:n";
+		String jpql = "SELECT u from User u where u.username = :n";
 		
 		try {
 			return em.createQuery(jpql,User.class).setParameter("n",username).getSingleResult();
 		} catch (Exception e) {
-			System.err.println("Invalid user name: "+username);
+			e.printStackTrace();
+			System.err.println("Invalid user name: " + username);
 			return null;
 		}
+	}
+	
+	
+	/* ----------------------------------------------------------------------------
+		findUserById
+	---------------------------------------------------------------------------- */
+	@Override
+	public User findUserById(int id) {
+		return (User)em.find(User.class, id);
+	}
+	
+
+	/* ----------------------------------------------------------------------------
+		sortUsersByCreationDate
+	---------------------------------------------------------------------------- */
+	@Override
+	public List<User> sortUsersByCreationDate(boolean ascendingOrder, int numberOfUsersToShow) {
+		String jpql = "SELECT u FROM User u ORDER BY u.creationDateTime";
+		
+		if (ascendingOrder) {
+			jpql += " ASC";
+			
+		} else {
+			jpql += " DESC";
+			
+		}
+		
+		List<User> users = em.createQuery(jpql, User.class).getResultList();
+		if (users != null && numberOfUsersToShow > 0) {
+			if (!users.isEmpty()) {
+				if (numberOfUsersToShow > users.size()) {
+					numberOfUsersToShow = users.size();
+				}
+				
+				users = users.subList(0, numberOfUsersToShow);
+			}
+			
+		} else {
+			users = new ArrayList<>();
+			
+		}
+		
+		return users;
+	}
+	public List<User> sortUsersByCreationDate(boolean ascendingOrder) {
+		String jpql = "SELECT u FROM User u ORDER BY u.creationDateTime";
+		
+		if (ascendingOrder) {
+			jpql += " ASC";
+			
+		} else {
+			jpql += " DESC";
+			
+		}
+		
+		List<User> users = em.createQuery(jpql, User.class).getResultList();
+		if (users == null) {
+			users = new ArrayList<>();
+		}
+		
+		return users;
 	}
 
 }
