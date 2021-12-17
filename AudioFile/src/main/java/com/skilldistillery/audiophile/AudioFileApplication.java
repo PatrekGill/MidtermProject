4ae5c7815ea9a8e5fcf98ec53a135fc08fb7f467
Kyle Palasti <Kyle.Palasti@gmail.com>
2021-12-17 14:09:50 -0700

    Deployed to Amazon AWS

--- AudioFile/src/main/java/com/skilldistillery/audiophile/AudioFileApplication.java
+++ AudioFile/src/main/java/com/skilldistillery/audiophile/AudioFileApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.audiophile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AudioFileApplication {
+public class AudioFileApplication extends SpringBootServletInitializer {
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(AudioFileApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(AudioFileApplication.class, args);
