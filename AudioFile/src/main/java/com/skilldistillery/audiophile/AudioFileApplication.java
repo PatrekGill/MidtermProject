75cf44885efa9f8a0a211e49b15beaaedbe7cc8f
PatrekGill <patrekGill@yahoo.com>
2021-12-27 23:59:15 -0700

    formatted code

--- AudioFile/src/main/java/com/skilldistillery/audiophile/AudioFileApplication.java
+++ AudioFile/src/main/java/com/skilldistillery/audiophile/AudioFileApplication.java
@@ -7,10 +7,10 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class AudioFileApplication extends SpringBootServletInitializer {
-	  @Override
-	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	    return application.sources(AudioFileApplication.class);
-	  }
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AudioFileApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AudioFileApplication.class, args);
